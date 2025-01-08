e76a34d94cbd977136ae781bb721a369a4c6f54c
ramadan <banworamadan@gmail.com>
2025-01-08 14:40:12 +0100

    initial commit

---
+++ age checker.py
@@ -0,0 +1,7 @@
+#this is a simple age checker script
+age = int(input('Enter your age:'))
+if age < 18:
+    print('sorry, you are not eligible to vote')#if the user's age is less than 18, it prints this
+else:
+    print('you are eligible to vote')#if user's age is 18 or more, it prints this
+#### i hope you  understand  this code and how it works    '''i only used if and else statement because there is no other criteria that needs to be checked'''
